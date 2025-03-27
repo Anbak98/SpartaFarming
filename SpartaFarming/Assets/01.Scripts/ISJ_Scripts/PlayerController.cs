61675399a134335e0ad4bcfd7fe233502d32e5fd
Seokjun Im <carnivorous5@gmail.com>
2025-03-27 13:39:33 +0900

    [ feat ] 플레이어 기본 이동 기능 추가

---
+++ SpartaFarming/Assets/01.Scripts/ISJ_Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 dir = transform.up * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;        
+
+        _rigidbody.velocity = dir;        
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+}
