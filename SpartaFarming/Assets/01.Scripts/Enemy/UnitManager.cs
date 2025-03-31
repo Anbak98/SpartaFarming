33915bd1563a75bdd5a38f2ebf304a68e4cc91ed
김지현 <kjhstar2486@naver.com>
2025-03-31 17:38:35 +0900

    [feat] UnitManager 수정

---
+++ SpartaFarming/Assets/01.Scripts/Enemy/UnitManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Unit
+{
+    public int hp;          // hp
+    public float speed;     // 속도
+    public float trackingTriggerRange;      // 서치범위 ( prowl -> tracking )
+    public float attackTriggerRange;        // 공격 범위 ( tracking -> Attack )
+    public float hitRange;                  // 히트 범위 
+    public float attackDamage;              // 공격 데미지 
+
+    public Unit(int hp, float speed, float searchRange, float attackRange, float hitRange , float damage)
+    {
+        this.hp                     = hp;
+        this.speed                  = speed;
+        this.trackingTriggerRange   = searchRange;
+        this.attackTriggerRange     = attackRange;
+        this.hitRange               = hitRange;
+        this.attackDamage = damage;
+    }
+}
+
+public class UnitManager : Singleton<UnitManager>
+{
+    // 나중에 csv 로 가져올 때 Dictionary<int, Unit> 이렇게 관리할 듯
+    private Dictionary<int, Unit> numberToUnit;
+
+    [SerializeField]
+    private Transform playerTrs;
+    [SerializeField]
+    private LayerMask obstacleLayer;
+    [SerializeField]
+    private LayerMask playerLayer;
+
+    public Transform PlayerTrs { get => playerTrs; }
+    public LayerMask ObstacleLayer { get => obstacleLayer;}
+    public LayerMask PlayerLayer { get => playerLayer; }
+
+    public Unit GetUnit(int num) { return numberToUnit[num]; }
+
+    private void Awake()
+    {
+        numberToUnit = new Dictionary<int, Unit>();
+
+        // ##TODO: 임시 생성 
+        Unit unit = new Unit(10, 3f, 5f, 1f, 2f, 10);
+        numberToUnit.Add(0,unit);
+    }
+}
