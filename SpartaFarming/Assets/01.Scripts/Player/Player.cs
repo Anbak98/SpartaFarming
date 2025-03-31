2bc32f63fcfc9fe639dd3bb35b0e6ab9172f4070
Seokjun Im <carnivorous5@gmail.com>
2025-03-31 19:37:31 +0900

    [ feat ] 플레이어 체력 추가

--- SpartaFarming/Assets/01.Scripts/Player/Player.cs
+++ SpartaFarming/Assets/01.Scripts/Player/Player.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerController Controller;
+    public PlayerCondition Condition;
 
     private void Awake()
     {
         GameManager.Instance.Player = this;
         Controller = GetComponent<PlayerController>();
+        Condition = GetComponent<PlayerCondition>();
     }
 }
