59349f8019c33d72c01c9ca41538bcf4ee3b91cb
Seokjun Im <carnivorous5@gmail.com>
2025-04-02 20:45:57 +0900

    [chore] 메인씬에 Merged_Test_ISJ 옮기기

--- SpartaFarming/Assets/01.Scripts/NPC.cs
+++ SpartaFarming/Assets/01.Scripts/NPC.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    // 범위 확인용 TriggerEnter
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
-        {
-            Debug.Log("상호작용 가능!");            
-        }
-    }
-
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
@@ -24,8 +13,7 @@ public class NPC : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
-        {
-            Debug.Log("상호작용 가능 범위 나감!");
+        {            
             GameManager.Instance.Player.Controller.canInteract = false;
         }
     }
